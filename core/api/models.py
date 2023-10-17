c9905b41517c3628c47c49bde5a4d7f30ce585a2
Anirudh <60171262+Blank333@users.noreply.github.com>
2023-10-17 14:44:02 +0530

    Created Todo django model
    Implemented CRUD for todo
    Implemented per task API
    Created endpoints for API

--- core/api/models.py
+++ core/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime, timedelta
 
-# Create your models here.
+class Todo(models.Model):
+    task = models.CharField(max_length = 256)
+    completed = models.BooleanField(default = False, blank = True)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    
+    deadline = models.DateTimeField(default= datetime.today() + timedelta(days=1))
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return self.task
--- core/api/urls.py
+++ core/api/urls.py
@@ -1,3 +1,7 @@
+from django.urls import path
+from .views import TodoListApiView, TodoApiView
 
 urlpatterns = [
+    path('todos/', TodoListApiView.as_view()),
+    path('todos/<int:todo_id>', TodoApiView.as_view()),
 ]
